f23677b0f0dc01e888e760f479fa677c63e51758
JungleColder <oneou6688@163.comm>
2016-08-30 22:59:42 +0800

    添加视差动画parallax_roll1和parallax2两个实例

--- js/parallax_roll.js
+++ js/parallax_roll.js
@@ -1,2 +1,19 @@
 
 <!--视差动画简单实例1-->
+
+var articleHeight =800;
+var section1 = document.getElementById('section1'),
+    section2 = document.getElementById('section2'),
+    section3 = document.getElementById('section3');
+window.addEventListener('scroll',scrollHandler);
+
+function scrollHandler(e){
+    var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+    if(scrollTop > 0 && scrollTop < articleHeight){
+        section1.classList.add('anim');
+    }else if(scrollTop >= articleHeight && scrollTop < articleHeight*2){
+        section2.classList.add('anim');
+    }else if(scrollTop >= articleHeight*2 && scrollTop < articleHeight*3){
+        section3.classList.add('anim');
+    }
+}
